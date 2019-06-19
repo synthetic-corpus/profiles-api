7e6416346cfc961bb5742009f4dfdc94f4c86810
unknown <joel@joelgonzaga.com>
2019-06-18 20:16:51 -0700

    created super user model.

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 
-class UserProfiles(AbstractBaseUser,PermissionsMixin):
+class UserProfileManager(BaseUserManager):
+    """ Manager for User Profiles """
+
+    def create_user(self,email,name,password=None):
+        """ create new user profile """
+        if not email:
+            raise ValueError('user must have an email address')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email,name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
     """Database Model for users in the system"""
     email = models.EmailField(max_length=255,unique=True)
     name = models.CharField(max_length=255)
@@ -26,4 +43,4 @@ class UserProfiles(AbstractBaseUser,PermissionsMixin):
     def __str__(self):
         """ Return string representation of user. """
         """ Not required, but recommended to avoid gibberish. """
-        return self.name
+        return self.email
