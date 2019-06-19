40e53d244a3b4240cb60ea13114bb741b16d282b
unknown <joel@joelgonzaga.com>
2019-06-18 19:57:35 -0700

    created a user model.

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
-# Create your models here.
+
+class UserProfiles(AbstractBaseUser,PermissionsMixin):
+    """Database Model for users in the system"""
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager() # Not yet Created
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = ['name']
+
+    def get_full_name(self):
+        """ Retrieve Full Name of user """
+        return self.name
+
+    def get_short_name(self):
+        """ Does the same thing as above. """
+        return self.name
+
+    def __str__(self):
+        """ Return string representation of user. """
+        """ Not required, but recommended to avoid gibberish. """
+        return self.name
