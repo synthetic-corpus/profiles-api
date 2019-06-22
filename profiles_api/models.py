78c8e80fd055e433f53d044e3a918296c9c5b2a8
unknown <joel@joelgonzaga.com>
2019-06-22 16:41:54 -0700

    modified settings and created custom user profile.

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -18,6 +18,18 @@ class UserProfileManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
 
+        return user
+    
+    def create_superuser(self,email,name,password):
+        """ Create and Save new superuser """
+        user = self.create_user(email,name,password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
 
 
 class UserProfile(AbstractBaseUser,PermissionsMixin):
