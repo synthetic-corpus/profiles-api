0dc572bf36d60c6b3b3a262f5cd3631f874a54e4
unknown <joel@joelgonzaga.com>
2019-06-24 20:08:17 -0700

    Enabled a django admin.

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from profiles_api import models
+
 # Register your models here.
+# Registering them here makes them accessible via an admin CLI account.
+
+admin.site.register(models.UserProfile)
